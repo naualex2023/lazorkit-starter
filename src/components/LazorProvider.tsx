04363ee1ede996732eff9d7431e8adf87b411cf9
Aleksandr Nedilko <bravo4022@gmail.com>
2026-01-06 01:33:00 +0300

    1.0.3

--- src/components/LazorProvider.tsx
+++ src/components/LazorProvider.tsx
@@ -9,7 +9,7 @@ export default function LazorProviderWrapper({ children }: { children: ReactNode
       rpcUrl={process.env.NEXT_PUBLIC_RPC_URL!}
       portalUrl={process.env.NEXT_PUBLIC_PORTAL_URL!}
       paymasterConfig={{
-        paymasterUrl: process.env.NEXT_PUBLIC_PAYMASTER_URL,
+        paymasterUrl: process.env.NEXT_PUBLIC_PAYMASTER_URL!,
         // Здесь можно добавить настройки для токенов (например, USDC), если нужно
       }}
     >
